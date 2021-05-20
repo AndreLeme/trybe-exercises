ac03299cc1aead20dd7172fc4a694d110e4302c2
Leme <andresleme@gmail.com>
2021-05-20 01:50:42 -0300

    Exercise/exercicio 2

--- fundamentos/bloco4-IntroJsLogicaProg/dia1-IntroPrimeirosPassos/exercises/script1.js
+++ fundamentos/bloco4-IntroJsLogicaProg/dia1-IntroPrimeirosPassos/exercises/script1.js
@@ -1,27 +1,27 @@
 let a = 7;
 let b = 77;
-let operacao;
+let operacao = 'modulo';
 
 switch (operacao) {
   case "adicao":
     console.log(a + b);
-    break
+    break;
 
   case "subtracao":
     console.log(a - b);
-    break
+    break;
 
   case "multiplicacao":
     console.log(a * b);
-    break
+    break;
 
   case "divisao":
     console.log(a / b);
-    break
+    break;
 
   case "modulo":
     console.log(a % b);
-    break
+    break;
   
   default:
     console.log("Operação inválida");
---
+++ fundamentos/bloco4-IntroJsLogicaProg/dia1-IntroPrimeirosPassos/exercises/script2.js
@@ -0,0 +1,10 @@
+let a = 7;
+let b = 77;
+
+if (a > b) {
+  console.log(a);
+} else if (a < b) {
+  console.log(b);
+} else {
+  console.log("Números iguais");
+}
