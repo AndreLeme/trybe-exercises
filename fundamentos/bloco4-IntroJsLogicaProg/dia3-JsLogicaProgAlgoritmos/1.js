520c01a99ce4e7062c2caf7b9fea1233c6abab2c
Leme <andresleme@gmail.com>
2021-05-21 20:18:01 -0300

    Legibilidade melhorada

--- fundamentos/bloco4-IntroJsLogicaProg/dia3-JsLogicaProgAlgoritmos/1.js
+++ fundamentos/bloco4-IntroJsLogicaProg/dia3-JsLogicaProgAlgoritmos/1.js
@@ -1,18 +1,20 @@
-let n = 5;
+let linhas = 8;
 let ast = "**";
 
 //[REQUISITO do exercicio] Verificar se n > 1
-if (n > 1) {
+if (linhas > 1) {
 
-  //Se n > 2 adicionar asteriscos equivalentes nas linhas
-  if (n > 2) {
-    for (let i = 3; i <= n; i +=1){
+  //Se linhas > 2 adicionar asteriscos equivalentes as linhas
+  if (linhas > 2) {
+    for (let totalAsteriscos = 3; totalAsteriscos <= linhas; totalAsteriscos +=1){
       ast += `*`
     };
   };
   
   //imprimir as linhas  
-  for (let i = 1; i <= n; i += 1) {
+  for (let i = 1; i <= linhas; i += 1) {
     console.log(`${ast}`);      
   };    
+} else {
+  console.log(`Número de linhas tem que ser maior que 1`);
 };
