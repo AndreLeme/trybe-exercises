e969b03ce1eeb374836274070e783e5ebdcb2933
Andre Leme <andresleme@gmail.com>
2021-06-23 20:52:40 -0300

    [COMPLETO] Ex 1

--- 1-fundamentos/bloco8-HofJs/dia1-IntroHof/exercises/1.js
+++ 1-fundamentos/bloco8-HofJs/dia1-IntroHof/exercises/1.js
@@ -1,15 +1,24 @@
 // Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com
 
-const newEmployees = (novaPessoaContratada) => {
+const newEmployees = (pessoa) => {
   const employees = {
-    id1: novaPessoaContratada('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: novaPessoaContratada('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: novaPessoaContratada('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: pessoa('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: pessoa('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: pessoa('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
 
-const novaPessoaContratada = (nomeCompleto) => `${nomeCompleto.toLowerCase().replace(/ /g, '_')}@trybe.com`;
+const novaPessoaContratada = (nomeCompleto) => {
+  const email = `${nomeCompleto.toLowerCase().replace(/ /g, '_')}@trybe.com`;
+  const pessoa = {
+    nomeCompleto: nomeCompleto,
+    email: email,
+  }
+  return pessoa;
+}
 
-console.log(novaPessoaContratada('Pedro Guerra'));
-// console.log(newEmployees.employees.id1);
+// console.log(novaPessoaContratada('Pedro Guerra'));
+
+console.log(newEmployees(novaPessoaContratada));
+
---
+++ 1-fundamentos/bloco8-HofJs/dia1-IntroHof/exercises/3.js
@@ -0,0 +1,4 @@
+// Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
