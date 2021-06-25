4d6044285368105863a6a0cb6e6a870276757e8a
Andre Leme <andresleme@gmail.com>
2021-06-25 18:35:34 -0300

    Exer 1

---
+++ 1-fundamentos/bloco8-HofJs/dia2-HofForeachFindSomeEverySort/content/find2.js
@@ -0,0 +1,8 @@
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista
+// Adicione seu código aqui:
+
+const findNameWithFiveLetters = () => names.find((element) => element.length === 5);
+
+console.log(findNameWithFiveLetters());
