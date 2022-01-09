5fbcfab45f67752414d777919c9ccd12f7fd96bc
Andre Leme <andresleme@gmail.com>
2022-01-09 17:14:34 -0300

    Exer 5

--- 3-back-end/bloco-22-introducao-desenvolvimento-web-node/dia-1-intro/exercises/imc.js
+++ 3-back-end/bloco-22-introducao-desenvolvimento-web-node/dia-1-intro/exercises/imc.js
@@ -1,5 +1,16 @@
-const imcCalculation = (weight, height) =>
-  (weight / (height ** 2)).toFixed(2);
+const imcCalculation = (weight, height) => {
+  const imc = (weight / (height ** 2)).toFixed(2);
+
+  let situation = '';
+  if (imc < 18,5) situation = 'Abaixo do peso (magreza)';
+  if (imc < 25) situation = 'Peso normal';
+  if (imc < 30) situation = 'Acima do peso (sobrepeso)';
+  if (imc < 35) situation = 'Obesidade grau I';
+  if (imc < 40) situation = 'Obesidade grau II';
+  if (imc >= 40) situation = 'Obesidade graus III e IV';
+
+  return `${imc}. ${situation}`;
+}
 
 module.exports = {
   imcCalculation,
