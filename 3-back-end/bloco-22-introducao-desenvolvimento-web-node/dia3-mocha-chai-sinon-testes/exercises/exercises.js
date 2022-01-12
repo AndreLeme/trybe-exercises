4af14a8d369b19a8c21d1ad98d387c33b1acbc40
Andre Leme <andresleme@gmail.com>
2022-01-11 21:11:59 -0300

    exercise 3

--- 3-back-end/bloco-22-introducao-desenvolvimento-web-node/dia3-mocha-chai-sinon-testes/exercises/exercises.js
+++ 3-back-end/bloco-22-introducao-desenvolvimento-web-node/dia3-mocha-chai-sinon-testes/exercises/exercises.js
@@ -1,4 +1,5 @@
 const tipoNumero = (num) => {
+  if (typeof num !== 'number') return 'Parâmetro precisa ser do tipo Number'
   if (num > 0) return 'positivo';
   if (num < 0) return 'negativo';
   if (num === 0) return 'neutro';
--- 3-back-end/bloco-22-introducao-desenvolvimento-web-node/dia3-mocha-chai-sinon-testes/exercises/tests/exercises.js
+++ 3-back-end/bloco-22-introducao-desenvolvimento-web-node/dia3-mocha-chai-sinon-testes/exercises/tests/exercises.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 
 const { tipoNumero } = require('../exercises');
 
-describe('Recebe número como param', () => {
+describe('Recebe número como parâmetro:', () => {
   describe('Quando número for maior que 0, a resposta', () => {
     it('é uma string', () => {
       const resposta = tipoNumero(5);
@@ -43,5 +43,19 @@ describe('Recebe número como param', () => {
 
       expect(resposta).to.be.equal('neutro');
     });
+  });
+});
+
+describe('Parâmetro recebido não for número, a resposta:', () => {
+  it('é uma string', () => {
+    const resposta = tipoNumero('ABC');
+
+    expect(resposta).to.be.a('string');
+  });
+
+  it('retorna "Parâmetro precisa ser do tipo Number"', () => {
+    const resposta = tipoNumero('ABC');
+
+    expect(resposta).to.be.equal('Parâmetro precisa ser do tipo Number');
   });
 });
