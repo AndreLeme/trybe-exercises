96e8c36a91b067dc38631dfe948f4916c89dee26
Andre Leme <andresleme@gmail.com>
2022-01-12 18:56:51 -0300

    exercise 3

--- 3-back-end/bloco-22-introducao-desenvolvimento-web-node/dia4-express-http/exercises/index.js
+++ 3-back-end/bloco-22-introducao-desenvolvimento-web-node/dia4-express-http/exercises/index.js
@@ -4,3 +4,21 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'pong' });
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ message: `Hello, ${name}!` });
+});
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if (parseInt(age, 10) <= 17) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  };
+
+  res.status(200).json({ message: `Hello, ${name}!` });
+});
