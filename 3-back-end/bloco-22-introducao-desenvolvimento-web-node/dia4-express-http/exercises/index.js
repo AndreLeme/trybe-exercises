3dbf888272912b3bfe966bf4f47902ee785963f5
Andre Leme <andresleme@gmail.com>
2022-01-13 14:35:16 -0300

    incompleto

--- 3-back-end/bloco-22-introducao-desenvolvimento-web-node/dia4-express-http/exercises/index.js
+++ 3-back-end/bloco-22-introducao-desenvolvimento-web-node/dia4-express-http/exercises/index.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
+
+const simpsonsUtils = require('./fs');
 
 const app = express();
 app.use(bodyParser.json());
 
+// exer 1
 app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
+// exer 2
 app.post('/hello', (req, res) => {
   const { name } = req.body;
   res.status(200).json({ message: `Hello, ${name}!` });
 });
 
+// exer 3
 app.post('/greetings', (req, res) => {
   const { name, age } = req.body;
 
@@ -23,8 +29,18 @@ app.post('/greetings', (req, res) => {
   res.status(200).json({ message: `Hello, ${name}!` });
 });
 
+// exer 4
 app.put('/users/:name/:age', (req, res) => {
   const { name, age } = req.params;
 
   res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade`})
 });
+
+// exer 5
+app.post(
+  '/simpsons',
+  rescue(async (req,res) => {
+    const { id, name } = req.body;
+
+    const simpsons = await simpsonsUtils
+  }))
